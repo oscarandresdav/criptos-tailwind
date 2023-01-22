90bdaeb2da5625c714026167c6f3e271596debb7
Oscar Davila <oscar7822002@gmail.com>
2023-01-22 07:43:07 -0500

    Agregando la imagen y el titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import ImagenCripto from "./assets/img/criptos-Traxer-Unsplash.jpg";
 
 function App() {
-  
   return (
-    <div>
-      <h1>Cotiza tu criptomoneda</h1>
+    <div className="md:flex h-screen items-center">
+      <div className="px-10 md:w-1/2">
+        <img
+          className="rounded-3xl shadow-xl shadow-cyan-700"
+          src={ImagenCripto}
+          alt=""
+        />
+      </div>
+      <div className="md:w-1/2">
+        <h1 className="text-4xl font-bold uppercase text-center underline underline-offset-8 decoration-8 decoration-red-400">
+          Cotiza tu criptomoneda
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
