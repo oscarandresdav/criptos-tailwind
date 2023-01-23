aa66220b5dd2a5c1394dbfda6855cc41364fb130
Oscar Davila <oscar7822002@gmail.com>
2023-01-22 20:13:19 -0500

    Consultando la API de CriptoCompare y agregando al select de nuestro CustomHook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import { monedas } from "../data/monedas";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 
 const Formulario = () => {
+  const [criptos, setCriptos] = useState([]);
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+  const [criptomoneda, SelectCriptomonedas] = useSelectMonedas("Elige tu criptomoneda", criptos);
+  useEffect(() => {
+    const consultarAPI = async() => {
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
 
-  console.log(moneda);
+      const arrayCripto = resultado.Data.map(cripto => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        }
+        return objeto;
+      });
+      setCriptos(arrayCripto);
+    }
+
+    consultarAPI();
+  }, [])
 
   return (
     <form>
       <SelectMonedas />
+      <SelectCriptomonedas />
       <input
         className="w-full text-2xl font-bold uppercase p-3 mt-8 bg-indigo-500 active:bg-indigo-700 hover:bg-indigo-600 hover:cursor-pointer"
         type="submit"
