9fd79cdabe455a01a577d2e715ad2d831f43ec99
Oscar Davila <oscar7822002@gmail.com>
2023-01-24 05:47:27 -0500

    Validando el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import ImagenCripto from "./assets/img/criptos-Traxer-Unsplash.jpg";
 import Formulario from "./components/Formulario";
 
 function App() {
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (error) {
+      window.addEventListener("click", () => setError(false));
+    }
+  }, [error]);
+
   return (
     <div className="md:flex h-screen items-center">
       <div className="px-10 md:w-1/2">
@@ -13,10 +22,13 @@ function App() {
       </div>
       <div className="mt-10 mx-8 md:w-1/2">
         <h1 className="text-2xl sm:text-4xl mb-10 font-bold uppercase text-center">
-          Cotiza tú <span className="font-mono lowercase rounded-lg text-indigo-700 bg-white px-1">criptomoneda</span>
+          Cotiza tú{" "}
+          <span className="font-mono lowercase rounded-lg text-indigo-700 bg-white px-1">
+            criptomoneda
+          </span>
         </h1>
-        
-        <Formulario />
+
+        <Formulario error={error} setError={setError} />
       </div>
     </div>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,39 +1,70 @@
 import { useEffect, useState } from "react";
 import { monedas } from "../data/monedas";
 import useSelectMonedas from "../hooks/useSelectMonedas";
+import Modal from "./Modal";
 
-const Formulario = () => {
+const Formulario = ({ error, setError }) => {
   const [criptos, setCriptos] = useState([]);
+
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
-  const [criptomoneda, SelectCriptomonedas] = useSelectMonedas("Elige tu criptomoneda", criptos);
+  const [criptomoneda, SelectCriptomonedas] = useSelectMonedas(
+    "Elige tu criptomoneda",
+    criptos
+  );
   useEffect(() => {
-    const consultarAPI = async() => {
-      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
-      const arrayCripto = resultado.Data.map(cripto => {
+      const arrayCripto = resultado.Data.map((cripto) => {
         const objeto = {
           id: cripto.CoinInfo.Name,
           nombre: cripto.CoinInfo.FullName,
-        }
+        };
         return objeto;
       });
       setCriptos(arrayCripto);
-    }
+    };
 
     consultarAPI();
-  }, [])
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+
+      return;
+    }
+
+    setError(false);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <SelectMonedas />
       <SelectCriptomonedas />
       <input
-        className="w-full text-2xl font-bold uppercase p-3 mt-8 bg-indigo-500 active:bg-indigo-700 hover:bg-indigo-600 hover:cursor-pointer"
+        className="w-full text-2xl font-bold uppercase rounded-lg p-3 mt-8 bg-indigo-500 active:bg-indigo-700 transition ease-in-out delay-150 hover:bg-indigo-600 hover:cursor-pointer hover:scale-105"
         type="submit"
         value="Cotizar"
       />
+      {error && (
+        <>
+          <div
+            className={
+              error
+                ? `block fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`
+                : `fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`
+            }
+          >
+            <Modal setError={setError} />
+          </div>
+        </>
+      )}
     </form>
   );
 };
